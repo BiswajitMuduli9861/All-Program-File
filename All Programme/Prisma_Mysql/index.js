e441ea6bfabdd3edc83c7548fddf6cf0e97a6d4e
Biswajit Muduli <bmuduli126@gmail.com>
2025-02-17 12:53:19 +0530

    17/2/2025

--- All Programme/Prisma_Mysql/index.js
+++ All Programme/Prisma_Mysql/index.js
@@ -4,48 +4,103 @@ const prisma = new PrismaClient();
 
 //function declaration
 const main =async()=>{
-    //Create (Insert Data)
-    //Single User
-    // const user = await prisma.user.create({   //by do be schema.prisma me table name || User || hoga to Database me show karega || user ||
-    //     data:{
-    //         name:"Sanjukta Muduli",
-    //         email:"sanjukta@gmail.com",
-    //     },
-    // })
-    // console.log(user)
+                                        //*****  Create (Insert Data) *****//
+                                        //****Single User ****//
 
 
-    //Multiple User
+    /*
+    const user = await prisma.user.create({   //by do be schema.prisma me table name || User || hoga to Database me show karega || user ||
+        data:{
+            name:"Sanjukta Muduli",
+            email:"sanjukta@gmail.com",
+        },
+    })
+    console.log(user)
+    */
 
-    // const users = await prisma.user.createMany({
-    //     data:[
-    //         {
-    //             name:"Labanyabati Muduli",
-    //             email:"labanyabati@gmail.com",
-    //         },
-    //         {
-    //             name:"Abhijit Muduli",
-    //             email:"abhijit@gmail.com",
-    //         }
-    //     ]
-    // })
-    // console.log(users)
+                                        //***** Insert Multiple User *****//
+
+    /*
+    const users = await prisma.user.createMany({
+        data:[
+            {
+                name:"Labanyabati Muduli",
+                email:"labanyabati@gmail.com",
+            },
+            {
+                name:"Abhijit Muduli",
+                email:"abhijit@gmail.com",
+            }
+        ]
+    })
+    console.log(users)
+    */
+
+                                    //***** Read (Fetch Data) *****//
+                                    //**** Get All Users ****//
+
+    /*
+    const users = await prisma.user.findMany(); // find all data
+    const users = await prisma.user.findFirst(); // find only first data
+    console.log(users)
+    */
+
+                                    //*****  Get a Single User by ID *****/
+
+    /*
+    const users = await prisma.user.findUnique({   //find Unique id jo schema me unique fields hoga kebala sia asiba jo unique heinathiba sia asibani
+        where:{id : 4}
+    })
+    console.log(users)
+    */
+
+                                    //***** Update (Modify Data) *****//
+                                    //**** Update One User ****/
+
+    /*
+    const updateUser = await prisma.user.update({
+        where: {id:4},            // accept only Unique fields nahale error asiba ||Argument `where` of type UserWhereUniqueInput needs at least one of `id` or `email` arguments. Available options are marked with ?.||
+        data:{name:"Abhijit Muduli"}
+    })
+    */
+
+                                    //***** Update Multiple Users ******//
+
+    /*
+    const updateUser = await prisma.user.updateMany({
+        where:{name:"Abhijit Muduli"},           // accept unique fields and non-unique fields
+        data:{email:"abhijit@gmail.com"}
+    })
+    console.log(updateUser)
+    */
+
+                                        //***** Delete (Remove Data) *****//
+                                        //**** Delete One User ****//
+
+    /*
+    const deleteUser = await prisma.user.delete({
+        where:{id:3}     //or where:[{},{}{}]
+    })
+    console.log(deleteUser)
+    */
 
 
-    //Read (Fetch Data)
-    //Get All Users
-    // const users = await prisma.user.findMany(); // find all data
-    // const users = await prisma.user.findFirst(); // find only first data
-    // console.log(users)
+                                        //***** Delete Multiple Users *****//
 
+    /*
+    const deleteUser = await prisma.user.deleteMany({
+        where:[{id:3},{id:"4"},{id:"5"}]      
+    })
+    console.log(deleteUser)
+    */
 
-    // Get a Single User by ID
-    // const users = await prisma.user.findUnique({   //find Unique id
-    //     where:{id : 4}
-    // })
-    // console.log(users)
+    
+                                    //***** Delete All Users *****//
 
-
+    /*
+    const deleteUser = await prisma.user.deleteMany({})
+    console.log(deleteUser)
+    */
 }
 
 // main() is function and call
